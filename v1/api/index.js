896421c65ce8f9c741cf213db0c06d2603b6c464
Benjamin Abbott <bmabbott@ncsu.edu>
2024-04-27 19:25:41 -0400

    tried to configure for vercel

--- v1/api/index.js
+++ v1/api/index.js
@@ -8,17 +8,9 @@ const PORT = 3000;
 // Designate the public folder as the location to look for static resources
 app.use(express.static('public'));
 
-const html_path = __dirname + './static/templates/'; // HTML files folder
-
-
 
 const path = require('path');
-const html_dir = path.join(__dirname, './static/templates/');
-
-router.get('/', (req, res) => {
-    res.sendFile(`${html_dir}form.html`);
-});
-
+const html_dir = path.join(__dirname, '../static/templates/');
 
 // Set up Middleware
 app.use(express.static('static'));
@@ -26,7 +18,7 @@ app.use(express.urlencoded({extended: true}));
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(html_path + 'index.html');
+  res.sendFile(html_dir + 'index.html');
 });
 
 // As our server to listen for incoming connections
